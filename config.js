65d6e1307a52e879cb9b387bf9bbb03634de42d0
Widya Ardianto <nartos9090@gmail.com>
2023-08-15 01:20:59 +0700

    detach config to a file

---
+++ config.js
@@ -0,0 +1,37 @@
+let ROW_HOUR_SCALE = 6
+let MINUTES_PER_CREDIT = 50
+let HOUR_START = 7
+let HOUR_END = 21
+
+let CREDIT_TO_SCALE = MINUTES_PER_CREDIT / (60 / ROW_HOUR_SCALE)
+
+// Days to show
+let days = [
+    'Senin',
+    'Selasa',
+    'Rabu',
+    'Kamis',
+    'Jumat',
+    // 'Sabtu',
+    // 'Minggu',
+]
+let BACKGROUNDS = [
+    ['#EF9A9A', '#FFCDD2'],
+    ['#F48FB1', '#F8BBD0'],
+    ['#CE93D8', '#E1BEE7'],
+    ['#B39DDB', '#D1C4E9'],
+    ['#9FA8DA', '#C5CAE9'],
+    ['#90CAF9', '#BBDEFB'],
+    ['#81D4FA', '#B3E5FC'],
+    ['#80DEEA', '#B2EBF2'],
+    ['#80CBC4', '#B2DFDB'],
+    ['#A5D6A7', '#C8E6C9'],
+    ['#C5E1A5', '#DCEDC8'],
+    ['#E6EE9C', '#F0F4C3'],
+    ['#FFF59D', '#FFF9C4'],
+    ['#FFE082', '#FFECB3'],
+    ['#FFCC80', '#FFE0B2'],
+    ['#FFAB91', '#FFCCBC'],
+    ['#BCAAA4', '#D7CCC8'],
+    ['#B0BEC5', '#CFD8DC'],
+]
